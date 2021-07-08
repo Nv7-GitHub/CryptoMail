39d304c6bd0be5c3d2b5d34f27110818c9be3acf
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-07-07 22:17:53 -0700

    Cleanup project, get ready for development

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CryptoMail
 E-mail, but with end-to-end encryption!
+
+# Getting Started
+After downloading, run
+```bash
+make install
+```
+
+After that, just run
+```
+make proto
+```
+Whenever the proto files are updated.
--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -9,10 +9,16 @@ import { CryptoMailClient, ServiceError } from '../pb/cryptomail_pb_service';
 import { AddRequest, AddResponse } from '../pb/cryptomail_pb';
 /* gRPC */
 
+export let client = new CryptoMailClient("http://localhost:8080");
+
 import { handleError } from './util';
 
+let p = document.createElement("p");
+p.innerText = "Loading 1+1...";
+p.classList.add("lead", "text-center");
+document.body.appendChild(p);
+
 /* Testing Code */
-let client = new CryptoMailClient("http://localhost:8080");
 let msg = new AddRequest();
 msg.setA(1);
 msg.setB(2);
@@ -20,11 +26,6 @@ client.add(msg, (err: ServiceError, resp: AddResponse) => {
   if (err != null) {
     handleError(err);
   }
-  console.log(resp.getC());
+  p.innerText = `1+1 is ${resp.getC()}`;
 })
 /* Testing Code */
-
-let p = document.createElement("p");
-p.innerText = "Hello! This is an example project!";
-p.classList.add("lead", "text-center");
-document.body.appendChild(p);
